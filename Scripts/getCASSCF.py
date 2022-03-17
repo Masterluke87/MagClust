c0464656cc3cfcc57589b01d8c89632c24368fa2
Masterluke <master.luke87@gmail.com>
2022-03-17 09:42:25 +0100

    CASSCF Script

--- Scripts/getCASSCF.py
+++ Scripts/getCASSCF.py
@@ -7,24 +7,25 @@ def getCASSCFEnergies(pathFile):
 
     
     c  = [c for c,x in enumerate(N) if "SA-CASSCF TRANSITION ENERGIES" in x ][0]
-    CAS = {"E"  : N[c+3].split()[7],
-           "C"  : N[c+3].split()[3],
-           "M"  : N[c+3].split()[5].replace(")",""),
-           "EX" : N[c+6].split()[3]}
+    Data = {"ID"     : pathFile,
+            "CAS-E"  : N[c+3].split()[7],
+            "CAS-C"  : N[c+3].split()[3],
+            "CAS-M"  : N[c+3].split()[5].replace(")",""),
+            "CAS-EX" : N[c+6].split()[3]}
     
     c  = [c for c,x in enumerate(N) if "NEVPT2 TRANSITION ENERGIES" in x ][0]
-    NEV = {"E"  : N[c+3].split()[7],
-           "C"  : N[c+3].split()[3].replace(",",""),
-           "M"  : N[c+3].split()[5].replace(")",""),
-           "EX" : N[c+6].split()[3]}
+    Data["NEV-E"]  =  N[c+3].split()[7]
+    Data["NEV-C"]  =  N[c+3].split()[3].replace(",","")
+    Data["NEV-M"]  =  N[c+3].split()[5].replace(")","")
+    Data["NEV-EX"] = N[c+6].split()[3]
 
     c = [c for c,x in enumerate(N) if "EFFECTIVE HAMILTONIAN SPIN-ORBIT COUPLING CONTRIBUTION" in x][0]
 
-    D      = float([x.split()[2] for x in N[c:c+200] if "D   =" in x][0])
-    EoverD = float([x.split()[2] for x in N[c:c+200] if "E/D =" in x][0])
+    Data["D"]      = float([x.split()[2] for x in N[c:c+200] if "D   =" in x][0])
+    Data["EoverD"] = float([x.split()[2] for x in N[c:c+200] if "E/D =" in x][0])
 
     
-    print(f"{pathFile} {CAS['C']} {CAS['M']} {CAS['E']} {CAS['EX']} {NEV['C']} {NEV['M']} {NEV['E']} {NEV['EX']} {D} {EoverD}")
+    return Data
 
 
 if __name__ == "__main__":
@@ -32,7 +33,7 @@ if __name__ == "__main__":
     with open("results.json") as f:
         D = json.load(f)
     for i in D["EnergyOrder"]:
-        getCASSCFEnergies(i)
+        exportData.append(getCASSCFEnergies(i))
 
     print(tabulate([list(x.values()) for x in exportData],headers=list(exportData[0].keys()),tablefmt="grid",floatfmt=".5f"))
 
